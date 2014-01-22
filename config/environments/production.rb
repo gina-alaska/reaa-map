7448ccf62398da0cb5dc2f26087fb148c5263eea
Will Fisher <will@teknofire.net>
2014-01-21 16:33:12 -0900

    allow to be embedded in aea site

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ ReaaMap::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_dispatch.default_headers['X-Frame-Options'] = 'ALLOW-FROM http://www.akenergyinventory.org'
 end
